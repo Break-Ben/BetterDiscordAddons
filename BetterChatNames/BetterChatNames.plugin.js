19a9b1a32f3fe05b7c7781e29748628cb23f9930
Break <80194912+Ben-Break@users.noreply.github.com>
2022-03-07 13:16:27 +0800

    Added many more chat name locations
    The chat names now get patched in mention autofill, search autofill and quick switcher autofill (with a toggle for all of them), as well as some dropdown menus in the settings. There were also some minor tweaks and capitalisation fixes

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by capitalising them, and removing dashes/underlines
- * @version 1.0.1
+ * @version 1.1.0
  * @authorLink https://github.com/Ben-Break
  * @website https://github.com/Ben-Break/BetterDiscordAddons
  * @source https://github.com/Ben-Break/BetterDiscordAddons/tree/main/BetterChatNames
@@ -10,18 +10,26 @@
  */
 
 const Patcher = BdApi.Patcher
-const dashRegExp = new RegExp("-|_", "g")
-const capitalRegExp = new RegExp(/(^\w{1})|(\W\w{1})/g)
+const DashRegex = new RegExp("-|_", "g")
+const CapitalRegex = new RegExp(/(^\w{1})|(\W\w{1})/g)
 const Channels = BdApi.findModule(m=>m?.default?.displayName === "ChannelItem")
 const Title = BdApi.findModule(m=>m?.default?.displayName === "HeaderBar")
 const Mention = BdApi.findModule(m=>m?.default?.displayName === "Mention")
 const Placeholder = BdApi.findModuleByDisplayName("SlateChannelTextArea").prototype
+const Dropdown = BdApi.findModuleByProps("SingleSelect")
 const Welcome = BdApi.findModule(m=>m?.default?.displayName === "TextChannelEmptyMessage")
+const MentionAutocomplete = BdApi.findModule(m=>m.default.displayName === "Autocomplete")
+const Search = BdApi.findModuleByProps("SearchPopoutComponent")
+const QuickSwitcher = BdApi.findModule(m=>m.Channel.displayName === "Channel")
 
+const PatchAutocomplete = true
+/*                         ↑↑ 
+Toggles changing the names of mention & search autocomplete (because you still need to type the dashes/underscores)
+*/
 module.exports = class BetterChatNames {
     start() {
         
-        // Channel names
+        // Chat names
         Patcher.after("BetterChatNames", Channels, "default", 
             (_, args, data)=>{
                 if(data?.props?.children?.props?.children?.[1]?.props?.children?.[0]?.props?.children?.[1]?.props?.children?.[0]?.props?.children){
@@ -48,31 +56,77 @@ module.exports = class BetterChatNames {
             }
         )
 
-        // Placeholder text
+        // Message placeholder
         Patcher.after("BetterChatNames", Placeholder, "render", 
             (_, args, data)=>{
-                if(data?._owner?.key == "enabled"){
+                if(data?.props?.children?.[1]?.props?.children?.[0] && data?._owner?.key == "enabled"){
                     data.props.children[1].props.children[0].props.children = this.patchText(data.props.children[1].props.children[0].props.children)
                 }
             }
         )
 
-        // Welcome to channel message   TODO: Stop from capitalising every word
-        Patcher.after("BetterChatNames", Welcome, "default", 
+        // Dropdown (in server settings)
+        Patcher.after("BetterChatNames", Dropdown, "SingleSelect",
             (_, args, data)=>{
-                if(data?.props?.children?.[2]?.props?.children?.[0]){
-                    data.props.children[2].props.children[0] = this.patchText(data.props.children[2].props.children[0])
-                }
-                if(data?.props?.children?.[1]?.props?.children){
-                    data.props.children[1].props.children = this.patchText(data.props.children[1].props.children)
+                if(data?.props?.options?.[1]?.channel?.type == 0 || data?.props?.options?.[0]?.label?.startsWith("#")) {
+                    data.props.options.forEach(element => {
+                        element.label = this.patchText(element.label)
+                    });
                 }
             }
         )
 
+        // 'Welcome to channel'
+        Patcher.after("BetterChatNames", Welcome, "default", 
+            (_, args, data)=>{
+                if(data?.props?.children?.[1]?.props?.children){
+                    var str = data.props.children[1].props.children
+                    data.props.children[1].props.children = str.substring(0, str.indexOf("#")) + this.patchText(str.substring(str.indexOf("#")))
+                }
+                if(data?.props?.children?.[2]?.props?.children?.[0]){
+                    var str = data.props.children[2].props.children[0]
+                    data.props.children[2].props.children[0] = str.substring(0, str.indexOf("#")) + this.patchText(str.substring(str.indexOf("#")))
+                }
+            }
+        )
+
+        if(PatchAutocomplete) {
+
+        // Mention Autocomplete
+        Patcher.after("BetterChatNames", MentionAutocomplete.default.Channel.prototype, "renderContent", 
+            (_, args, data)=>{
+                if(data) {
+                    data.props.children[1].props.children.props.children = this.patchText(data.props.children[1].props.children.props.children)
+                } 
+            }
+        )
+        
+        // Search Autocomplete
+        Patcher.after("BetterChatNames", Search.GroupData.FILTER_IN, "component", 
+            (_, args, data)=>{
+                if(data) {
+                    Patcher.after("BetterChatNames(Searchbar)", data.props, "renderResult", 
+                        (_, args, data)=>{
+                            data.props.children[1].props.children = this.patchText(data.props.children[1].props.children)
+                        }
+                    )
+                } 
+            }
+        )
+
+        // Quick Switcher Autocomplete
+        Patcher.after("BetterChatNames", QuickSwitcher.Channel.prototype, "renderName",
+            (_, args, data)=>{
+                if(data) {
+                    data.props.children[0].props.children = this.patchText(data.props.children[0].props.children)
+                } 
+            }
+        )}
+
         this.reloadGuild()
     }
 
-    patchText(channelName) { return channelName.replace(dashRegExp, " ").replace(capitalRegExp, letter => letter.toUpperCase()) } // Remove dash + underscore, then capitalise
+    patchText(channelName) { return channelName.replace(DashRegex, " ").replace(CapitalRegex, letter => letter.toUpperCase()) } // Remove dash + underscore, then capitalise
 
     reloadGuild() {
         const currentGuildId = BdApi.findModuleByProps("getLastSelectedGuildId").getGuildId()
