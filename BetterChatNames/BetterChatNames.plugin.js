28b00f5b527f66c41768d4afd62ecc0fb62454cc
Break <80194912+Break-Ben@users.noreply.github.com>
2023-02-19 15:12:13 +0800

    Fixes + Formatting (1.4.2)
    Channel name patching has been fixed and I've finally added chat placeholder patching back

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by automatically capitalising them, and removing dashes & underscores
- * @version 1.4.1
+ * @version 1.4.2
  * @authorLink https://github.com/Break-Ben
  * @website https://github.com/Break-Ben/BetterDiscordAddons
  * @source https://github.com/Break-Ben/BetterDiscordAddons/tree/main/BetterChatNames
@@ -14,38 +14,39 @@ const RemoveDashes = true
 // ↑ ↑ ↑ ↑ Settings ↑ ↑ ↑ ↑
 
 var titleObserver
-const DashRegex = new RegExp("-|_", "g")
+const DashRegex = new RegExp('-|_', 'g')
 const CapitalRegex = new RegExp(/(^\w)|([^a-zA-ZÀ-ɏḀ-ỿ'’]\w)/g)
-const {Webpack, Patcher} = new BdApi("BetterChatNames")
-const {getModule, Filters} = Webpack
-const {byProps} = Filters
-const CurrentServer = getModule(byProps("getLastSelectedGuildId"))
-const CurrentChannel = getModule(byProps("getLastSelectedChannelId"))
-const TransitionTo = getModule(m => m?.toString?.().includes('"transitionTo - Transitioning to "'), {searchExports: true})
+const { Webpack, Patcher } = new BdApi('BetterChatNames')
+const { getModule, Filters } = Webpack
+const { byProps, byStrings } = Filters
+const CurrentServer = getModule(byProps('getLastSelectedGuildId'))
+const CurrentChannel = getModule(byProps('getLastSelectedChannelId'))
+const TransitionTo = getModule(byStrings('"transitionTo - Transitioning to "'), { searchExports: true })
 
-const Channels = getModule(m => Object.values(m).some(v => v?.toString?.().includes(".SELECTED")))
-const Title = getModule(m => Object.values(m).some(v => v?.toString?.().includes(".toolbar")))
-const Mention = getModule(m => Object.values(m).some(v => v?.toString?.().includes(".iconMention")))
+const Channels = getModule(m => Object.values(m).some(byStrings('.SELECTED')))
+const Title = getModule(m => Object.values(m).some(byStrings('.toolbar')))
+const Placeholder = getModule(m => m.type?.render && byStrings('.richValue,', ',submitButtonVisible:(null===(')(m.type?.render)).type
+const Mention = getModule(m => Object.values(m).some(byStrings('.iconMention')))
 
 module.exports = class BetterChatNames {
     patchNames() {
         // Chat names
-        Patcher.after(Channels, "Z", 
-            (_, args, data)=>{
-                if(data?.props?.children?.props?.children?.[1]?.props?.children?.[0]?.props?.children?.[1]?.props?.children?.[0]?.props?.children){
-                    data.props.children.props.children[1].props.children[0].props.children[1].props.children[0].props.children = this.patchText(data.props.children.props.children[1].props.children[0].props.children[1].props.children[0].props.children)
+        Patcher.after(Channels, 'Z',
+            (_, args, data) => {
+                if (data?.props?.children?.props?.children?.[1]?.props?.children?.[0]?.props?.children?.[1]?.props?.children?.[0]) {
+                    data.props.children.props.children[1].props.children[0].props.children[1].props.children[0] = this.patchText(data.props.children.props.children[1].props.children[0].props.children[1].props.children[0])
                 }
             }
         )
 
         // Title
-        Patcher.after(Title, "ZP", 
-            (_, args, data)=>{
+        Patcher.after(Title, 'ZP',
+            (_, args, data) => {
                 const TitleBar = data?.props?.children?.props?.children?.[0]?.props?.children[1]
-                if(TitleBar?.[1]?.props?.guild) {var n = 0} //If in a server
-                else if(TitleBar?.[2]?.props?.guild) {var n = 1} //If in a server with 'Hide Channels' installed
-                if(n != null) {
-                    if(TitleBar[n+1].props.channel?.type == 11) { //If in a thread
+                if (TitleBar?.[1]?.props?.guild) { var n = 0 } //If in a server
+                else if (TitleBar?.[2]?.props?.guild) { var n = 1 } //If in a server with 'Hide Channels' installed
+                if (n != null) {
+                    if (TitleBar[n + 1].props.channel?.type == 11) { //If in a thread
                         data.props.children.props.children[0].props.children[1][n].props.children[0].props.children[1].props.children = this.patchText(TitleBar[n].props.children[0].props.children[1].props.children)
                     }
                     else { //If in chat/forum
@@ -55,11 +56,20 @@ module.exports = class BetterChatNames {
             }
         )
 
+        // Chat placeholder
+        Patcher.after(Placeholder, 'render',
+            (_, args, data) => {
+                if (data?.props?.children?.props?.children?.[1]?.props?.children?.[0]?.props?.channel?.guild_id && !data?.props?.children?.props?.children?.[0]?.props?.editorRef?.current?.props?.disabled) {//If in a server and can message
+                    data.props.children.props.children[1].props.children[1].props.children[2].props.children[1].props.children.props.placeholder = this.patchText(data.props.children.props.children[1].props.children[1].props.children[2].props.children[1].props.children.props.placeholder)
+                }
+            }
+        )
+
         // Chat mention
-        Patcher.after(Mention, "Z", 
-            (_, args, data)=>{
-                if(data?.props?.children?.[0]){ //If a chat mention
-                    if(data.props.role == "link") { //If in chat
+        Patcher.after(Mention, 'Z',
+            (_, args, data) => {
+                if (data?.props?.children?.[0]) { //If a chat mention
+                    if (data.props.role == 'link') { //If in chat
                         data.props.children[1][0] = this.patchText(data.props.children[1][0])
                     }
                     else { //If in text area
@@ -73,29 +83,29 @@ module.exports = class BetterChatNames {
     // App title
     patchTitle() {
         const patchedTitle = this.patchText(document.title)
-        if(CurrentServer?.getGuildId() && document.title != patchedTitle) { //If in server and title not already patched
+        if (CurrentServer?.getGuildId() && document.title != patchedTitle) { //If in server and title not already patched
             document.title = patchedTitle
         }
     }
 
     patchText(channelName) {
-        if(RemoveDashes) {channelName = channelName.replace(DashRegex, " ")}
-        if(Capitalise) {channelName = channelName.replace(CapitalRegex, letter => letter.toUpperCase())}
+        if (RemoveDashes) { channelName = channelName.replace(DashRegex, ' ') }
+        if (Capitalise) { channelName = channelName.replace(CapitalRegex, letter => letter.toUpperCase()) }
         return channelName
     }
 
     reloadServer() {
         const currentServer = CurrentServer?.getGuildId()
         const currentChannel = CurrentChannel?.getChannelId()
-        if(currentServer) { //If not in a DM
+        if (currentServer) { //If not in a DM
             TransitionTo('/channels/@me')
-            setImmediate(()=>TransitionTo(`/channels/${currentServer}/${currentChannel}`))
+            setImmediate(() => TransitionTo(`/channels/${currentServer}/${currentChannel}`))
         }
     }
 
     start() {
         titleObserver = new MutationObserver(_ => { this.patchTitle(); })
-        titleObserver.observe(document.querySelector('title'), {childList: true})
+        titleObserver.observe(document.querySelector('title'), { childList: true })
         this.patchNames()
         this.reloadServer()
     }
