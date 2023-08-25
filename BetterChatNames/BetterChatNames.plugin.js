5f8dacaaf56008470b3dd061eb853573c677866c
Break <80194912+Break-Ben@users.noreply.github.com>
2023-08-25 17:26:47 +0800

    Fixes (1.5.2)
    Fixed title bar patching

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by automatically capitalising them, and removing dashes & underscores
- * @version 1.5.1
+ * @version 1.5.2
  * @authorLink https://github.com/Break-Ben
  * @website https://github.com/Break-Ben/BetterDiscordAddons
  * @source https://github.com/Break-Ben/BetterDiscordAddons/tree/main/BetterChatNames
@@ -42,15 +42,14 @@ module.exports = class BetterChatNames {
         // Title
         Patcher.after(Title, 'ZP',
             (_, args, data) => {
-                const titleBar = data?.props?.children?.props?.children?.[0]?.props?.children[1]
-                if (titleBar?.[1]?.props?.guild) { var n = 0 } //If in a server
-                else if (titleBar?.[2]?.props?.guild) { var n = 1 } //If in a server with 'Hide Channels' installed
+                const titleBar = data?.props?.children?.props?.children?.[0]?.props?.children?.[0]?.props?.children?.filter(c => c)[0]
+                const n = titleBar[1]?.props?.guild ? 0 : (titleBar[2]?.props?.guild ? 1 : null) //If in a server with 'Hide Channels' not installed
                 if (n != null) {
                     if (titleBar[n + 1].props.channel?.type == 11) { //If in a thread
-                        data.props.children.props.children[0].props.children[1][n].props.children.filter(c => c)[0].props.children[1].props.children = this.patchText(titleBar[n].props.children.filter(c => c)[0].props.children[1].props.children)
+                        data.props.children.props.children[0].props.children[0].props.children.filter(c => c)[0][n].props.children.filter(c => c)[0].props.children[1].props.children = this.patchText(titleBar[n].props.children.filter(c => c)[0].props.children[1].props.children)
                     }
                     else { //If in chat/forum
-                        data.props.children.props.children[0].props.children[1][n].props.children.filter(c => c)[1].props.children.props.children[2] = this.patchText(titleBar[n].props.children.filter(c => c)[1].props.children.props.children[2])
+                        data.props.children.props.children[0].props.children[0].props.children.filter(c => c)[0][n].props.children[3].props.children.props.children[2] = this.patchText(titleBar[n].props.children[3].props.children.props.children[2])
                     }
                 }
             }
@@ -104,7 +103,7 @@ module.exports = class BetterChatNames {
     }
 
     start() {
-        titleObserver = new MutationObserver(_ => { this.patchTitle(); })
+        titleObserver = new MutationObserver(_ => { this.patchTitle() })
         titleObserver.observe(document.querySelector('title'), { childList: true })
         this.patchNames()
         this.reloadServer()
