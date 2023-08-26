a8d799999d9eefcd41cf950cb091e5cb1601c8fc
Break <80194912+Break-Ben@users.noreply.github.com>
2023-08-26 12:18:49 +0800

    Cleaned Code (1.5.3)
    Cleaned/simplified a lot of the code and moved from byProps to byKeys

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by automatically capitalising them, and removing dashes & underscores
- * @version 1.5.2
+ * @version 1.5.3
  * @authorLink https://github.com/Break-Ben
  * @website https://github.com/Break-Ben/BetterDiscordAddons
  * @source https://github.com/Break-Ben/BetterDiscordAddons/tree/main/BetterChatNames
@@ -18,9 +18,9 @@ const DashRegex = new RegExp('-|_', 'g')
 const CapitalRegex = new RegExp(/(?<=(^|[^\p{L}'’]))\p{L}/gu)
 const { Webpack, Patcher } = new BdApi('BetterChatNames')
 const { getModule, Filters } = Webpack
-const { byProps, byStrings } = Filters
-const CurrentServer = getModule(byProps('getLastSelectedGuildId'))
-const CurrentChannel = getModule(byProps('getLastSelectedChannelId'))
+const { byKeys, byStrings } = Filters
+const CurrentServer = getModule(byKeys('getLastSelectedGuildId'))
+const CurrentChannel = getModule(byKeys('getLastSelectedChannelId'))
 const TransitionTo = getModule(byStrings('"transitionTo - Transitioning to "'), { searchExports: true })
 
 const Channels = getModule(m => Object.values(m).some(byStrings('.SELECTED')))
@@ -31,52 +31,43 @@ const Mention = getModule(m => Object.values(m).some(byStrings('.iconMention')))
 module.exports = class BetterChatNames {
     patchNames() {
         // Chat names
-        Patcher.after(Channels, 'Z',
-            (_, args, data) => {
-                if (data.props?.children?.props?.children?.[1]?.props?.children?.props?.children?.filter(c => c)[0]?.props?.children?.filter(c => c)[1]?.props?.children) {
-                    data.props.children.props.children[1].props.children.props.children.filter(c => c)[0].props.children.filter(c => c)[1].props.children = this.patchText(data.props.children.props.children[1].props.children.props.children.filter(c => c)[0].props.children.filter(c => c)[1].props.children)
-                }
+        Patcher.after(Channels, 'Z', (_, args, data) => {
+            const channel = data.props?.children?.props?.children?.[1]?.props?.children?.props?.children?.filter(c => c)[0]?.props?.children?.filter(c => c)[1]?.props
+            if (channel) {
+                channel.children = this.patchText(channel.children)
             }
-        )
+        })
 
         // Title
-        Patcher.after(Title, 'ZP',
-            (_, args, data) => {
-                const titleBar = data?.props?.children?.props?.children?.[0]?.props?.children?.[0]?.props?.children?.filter(c => c)[0]
-                const n = titleBar[1]?.props?.guild ? 0 : (titleBar[2]?.props?.guild ? 1 : null) //If in a server with 'Hide Channels' not installed
-                if (n != null) {
-                    if (titleBar[n + 1].props.channel?.type == 11) { //If in a thread
-                        data.props.children.props.children[0].props.children[0].props.children.filter(c => c)[0][n].props.children.filter(c => c)[0].props.children[1].props.children = this.patchText(titleBar[n].props.children.filter(c => c)[0].props.children[1].props.children)
-                    }
-                    else { //If in chat/forum
-                        data.props.children.props.children[0].props.children[0].props.children.filter(c => c)[0][n].props.children[3].props.children.props.children[2] = this.patchText(titleBar[n].props.children[3].props.children.props.children[2])
-                    }
+        Patcher.after(Title, 'ZP', (_, args, data) => {
+            const titleBar = data?.props?.children?.props?.children?.[0]?.props?.children?.[0]?.props?.children?.filter(c => c)[0]
+            const n = titleBar[1]?.props?.guild ? 0 : (titleBar[2]?.props?.guild ? 1 : null) //If in a server with 'Hide Channels' not installed
+            if (n != null) {
+                if (titleBar[n + 1].props.channel?.type == 11) { //If in a thread
+                    titleBar[n].props.children.filter(c => c)[0].props.children[1].props.children = this.patchText(titleBar[n].props.children.filter(c => c)[0].props.children[1].props.children)
+                }
+                else { //If in chat/forum
+                    titleBar[n].props.children[3].props.children.props.children[2] = this.patchText(titleBar[n].props.children[3].props.children.props.children[2])
                 }
             }
-        )
+        })
 
         // Chat placeholder
-        Patcher.after(Placeholder, 'render',
-            (_, args, data) => {
-                if (data?.props?.children?.props?.children?.[1]?.props?.children?.[0]?.props?.channel?.guild_id && !data?.props?.children?.props?.children?.[0]?.props?.editorRef?.current?.props?.disabled) {//If in a server and can message
-                    data.props.children.props.children[1].props.children[1].props.children[2].props.children[1].props.children.props.placeholder = this.patchText(data.props.children.props.children[1].props.children[1].props.children[2].props.children[1].props.children.props.placeholder)
-                }
+        Patcher.after(Placeholder, 'render', (_, args, data) => {
+            const textArea = data?.props?.children?.props?.children?.[1]?.props?.children
+            if (textArea?.[0]?.props?.channel?.guild_id && !textArea?.[0]?.props?.editorRef?.current?.props?.disabled) {//If in a server and can message
+                const placeholder = textArea[1]?.props?.children?.[2]?.props?.children?.[1]?.props?.children?.props
+                placeholder.placeholder = this.patchText(placeholder.placeholder)
             }
-        )
+        })
 
         // Chat mention
-        Patcher.after(Mention, 'Z',
-            (_, args, data) => {
-                if (data?.props?.children?.[1]) {
-                    if (Array.isArray(data?.props?.children?.[1].props?.children)) { //If in chat
-                        data.props.children[1].props.children[0].props.children = this.patchText(data.props.children[1].props.children[0].props.children)
-                    }
-                    else { //If in textarea
-                        data.props.children[1].props.children = this.patchText(data.props.children[1].props.children)
-                    }
-                }
+        Patcher.after(Mention, 'Z', (_, args, data) => {
+            const mention = data?.props?.children?.[1].props?.children?.[0]?.props || data?.props?.children?.[1]?.props; //If in chat or text area
+            if (mention) {
+                mention.children = this.patchText(mention.children)
             }
-        )
+        })
     }
 
     // App title
@@ -93,7 +84,7 @@ module.exports = class BetterChatNames {
         return channelName
     }
 
-    reloadServer() {
+    refreshChannel() {
         const currentServer = CurrentServer?.getGuildId()
         const currentChannel = CurrentChannel?.getChannelId()
         if (currentServer) { //If not in a DM
@@ -106,12 +97,12 @@ module.exports = class BetterChatNames {
         titleObserver = new MutationObserver(_ => { this.patchTitle() })
         titleObserver.observe(document.querySelector('title'), { childList: true })
         this.patchNames()
-        this.reloadServer()
+        this.refreshChannel()
     }
 
     stop() {
         titleObserver.disconnect()
         Patcher.unpatchAll()
-        this.reloadServer()
+        this.refreshChannel()
     }
 }
