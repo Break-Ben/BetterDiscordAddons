4838c8af8e9597b96be8026d366ea0f79854cf23
Break <80194912+Break-Ben@users.noreply.github.com>
2023-12-19 12:26:51 +0800

    Option to toggle voice/thread patching (1.6.0)
    • Added an option that allows you to toggle the patching of channels with unrestricted naming conditions. These channels, including threads, voice channels and stages, can already include spaces and capital letters. Note that this feature is not perfectly complete, but what has been added is fully functional.
    • Fixed compatibility with the HideChannels plugin
    • This version also includes some small styling changes

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by automatically capitalising them, and removing dashes & underscores
- * @version 1.5.6
+ * @version 1.6.0
  * @authorLink https://github.com/Break-Ben
  * @website https://github.com/Break-Ben/BetterDiscordAddons
  * @source https://github.com/Break-Ben/BetterDiscordAddons/tree/main/BetterChatNames
@@ -11,7 +11,7 @@
 
 const Capitalise = true
 const RemoveDashes = true
-const CapitaliseVoiceChannels = false
+const PatchUnrestrictedChannels = true  // Also change the names of channels including threads, voice channels and stages that can already contain capitals and spaces
 // ↑ ↑ ↑ ↑ Settings ↑ ↑ ↑ ↑
 
 var titleObserver
@@ -32,67 +32,53 @@ module.exports = class BetterChatNames {
     patchNames() {
         // Chat names
         Patcher.after(Channels, 'default', (_, args, data) => {
-            const baseChannel =
-                data.props?.children?.props?.children?.[1]?.props?.children?.props?.children
-                    ?.find((c) => c)
-                    ?.props?.children?.filter((c) => c)
+            const channel = data.props?.children?.props?.children?.[1]?.props?.children?.props?.children?.find(c => c)?.props?.children?.filter(c => c)
+            const channelName = channel?.[1]?.props
 
-            const channelData = baseChannel?.[0]?.props?.channel // Channel info like channel type, channel ID, server ID, etc
-            const channel = baseChannel?.[1]?.props // DOM information like classes, titles, etc
-
-            // Don't change voice channel names unless you enable it
-            if (channel && (![2, 13].includes(channelData.type) || CapitaliseVoiceChannels)) {
-                channel.children = this.patchText(channel.children)
+            if (channelName && (![2, 13].includes(channel?.[0]?.props?.channel?.type) || PatchUnrestrictedChannels)) { // If not a voice/stage channel or PatchUnrestrictedChannels is enabled
+                channelName.children = this.patchText(channelName.children)
             }
         })
 
         // Toolbar Title
         Patcher.after(Title, 'default', (_, args, data) => {
-            const titleBar = data?.props?.children?.props?.children
-            const n = titleBar[1]?.props?.guild ? 0 : titleBar[2]?.props?.guild ? 1 : null // If in a server with 'Hide Channels' not installed
-            if (n == null) {
-                return
-            }
+            const titleBar = data?.props?.children?.props?.children?.filter(c => c)
+            const n = titleBar[1]?.props?.guild ? 0 : titleBar[2]?.props?.guild ? 1 : null // If in a server with 'Hide Channels' installed
+
+            if (n == null) { return }
 
             if (titleBar[n + 1].props.channel?.type == 11) { // If in a thread
-                const title = titleBar[n].props.children.filter((c) => c)[0]
-                    .props.children[1].props.children
-                title = this.patchText(title)
-                return
+                titleBar[n].props.children.find(c => c).props.children[1].props.children = this.patchText(titleBar[n].props.children.find(c => c).props.children[1].props.children)
+                if (PatchUnrestrictedChannels) {
+                    titleBar[n].props.children.filter(c => c)[2].props.children.props.children[2] = this.patchText(titleBar[n].props.children.filter(c => c)[2].props.children.props.children[2])
+                }
             }
-
-            const chatName = titleBar?.[n]?.props?.children?.[3]?.props?.children?.props // If in chat/forum
-
-            if (!chatName) { // If channel patched with EditChannels
-                const title = titleBar[n].props.children[3].props.children
-                title = this.patchText(title)
-                return
+            else { // If in chat/forum
+                const channelName = titleBar?.[n]?.props?.children?.[3]?.props?.children?.props
+                if (channelName) { // If channel not patched with EditChannels
+                    channelName.children[2] = this.patchText(channelName.children[2])
+                }
+                else {
+                    titleBar[n].props.children[3].props.children = this.patchText(titleBar[n].props.children[3].props.children)
+                }
             }
-
-            chatName.children[2] = this.patchText(chatName.children[2])
         })
 
         // Chat placeholder
         Patcher.after(Placeholder, 'render', (_, args, data) => {
             const textarea = data?.props?.children?.[2]?.props
-            if ( // If in a server, not in a thread, can message and not editing a message
-                textarea?.channel?.guild_id &&
-                textarea.channel.type != 11 &&
-                !textarea?.disabled &&
-                textarea?.type?.analyticsName == 'normal'
-            ) {
+
+            if (textarea?.channel?.guild_id && (textarea?.channel?.type != 11 || PatchUnrestrictedChannels) && !textarea?.disabled && textarea?.type?.analyticsName == 'normal') {// If in a server, not in a thread (or PatchUnrestrictedChannels is enabled), can message and not editing a message
                 textarea.placeholder = this.patchText(textarea.placeholder)
             }
         })
 
         // Chat mention
         Patcher.after(Mention, 'default', (_, args, data) => {
-            const mention = // If in chat or text area
-                data?.props?.children?.[1].props?.children?.[0]?.props ||
-                data?.props?.children?.[1]?.props
+            const channelName = data?.props?.children?.[1].props?.children?.[0]?.props || data?.props?.children?.[1]?.props // If in chat or text area
 
-            if (mention) {
-                mention.children = this.patchText(mention.children)
+            if (channelName && (data.props.className.includes('iconMentionText') || PatchUnrestrictedChannels)) { // If a normal chat mention or PatchUnrestrictedChannels is enabled
+                channelName.children = this.patchText(channelName.children)
             }
         })
     }
@@ -100,20 +86,22 @@ module.exports = class BetterChatNames {
     // App title
     patchTitle() {
         const patchedTitle = this.patchText(document.title)
+
         if (CurrentServer?.getGuildId() && document.title != patchedTitle) { // If in server and title not already patched
             document.title = patchedTitle
         }
     }
 
     patchText(channelName) {
-        if (RemoveDashes) channelName = channelName.replace(DashRegex, ' ')
-        if (Capitalise) channelName = channelName.replace(CapitalRegex, (letter) => letter.toUpperCase())
+        if (RemoveDashes) { channelName = channelName.replace(DashRegex, ' ') }
+        if (Capitalise) { channelName = channelName.replace(CapitalRegex, letter => letter.toUpperCase()) }
         return channelName
     }
 
     refreshChannel() {
         const currentServer = CurrentServer?.getGuildId()
         const currentChannel = CurrentChannel?.getChannelId()
+
         if (currentServer) { // If not in a DM
             TransitionTo('/channels/@me')
             setImmediate(() => TransitionTo(`/channels/${currentServer}/${currentChannel}`))
@@ -122,12 +110,13 @@ module.exports = class BetterChatNames {
 
     start() {
         var lastUnpatchedAppTitle
-        titleObserver = new MutationObserver((_) => {
+        titleObserver = new MutationObserver(_ => {
             if (document.title != lastUnpatchedAppTitle) { // Resolves conflicts with EditChannels' MutationObserver
                 lastUnpatchedAppTitle = document.title
                 this.patchTitle()
             }
         })
+
         titleObserver.observe(document.querySelector('title'), { childList: true })
         this.patchNames()
         this.refreshChannel()
