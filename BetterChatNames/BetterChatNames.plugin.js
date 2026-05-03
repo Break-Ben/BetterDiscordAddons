4ac59241b04894b7801fe228f9de5da8425aff0a
Break <80194912+Break-Ben@users.noreply.github.com>
2026-05-03 12:51:39 +0800

    Lazy loading fix (1.9.1)
    Moved all modules to a 'modules' object so that waitForModule can be easily used when required. This fixes the plugin no longer working on app startup after the recent Discord update

--- BetterChatNames/BetterChatNames.plugin.js
+++ BetterChatNames/BetterChatNames.plugin.js
@@ -2,7 +2,7 @@
  * @name BetterChatNames
  * @author Break
  * @description Improves chat names by automatically capitalising them and/or removing dashes/underscores
- * @version 1.9.0
+ * @version 1.9.1
  * @authorLink https://github.com/Break-Ben
  * @website https://github.com/Break-Ben/BetterDiscordAddons
  * @source https://github.com/Break-Ben/BetterDiscordAddons/tree/main/BetterChatNames
@@ -28,27 +28,20 @@ const regex = {
     word: /\b\p{L}+\b/gu,
     emoji: /([-_]?\p{Extended_Pictographic}[-_┃]?)|(\uFE0F\u20E3)/gu
 }
-
-let titleObserver
-const { Webpack, Patcher, Utils, Data, UI } = new BdApi('BetterChatNames')
-const { getModule, getByStrings, getByKeys, getByPrototypeKeys } = Webpack
-const { findInTree } = Utils
-const searchOptions = { walkable: ['children', 'props'] } // For Utils.findInTree
-
-const currentServer = getByKeys('getLastSelectedGuildId')
-const currentChannel = getByKeys('getLastSelectedChannelId')
-const transitionTo = getByStrings('transitionTo - Transitioning to', { searchExports: true })
-
-const sidebar = getModule(m => m?.render?.toString()?.includes('.CHANNEL'))
-const header = getByStrings('.HEADER_BAR)', { defaultExport: false })
-const placeholder = getByPrototypeKeys('getPlaceholder').prototype
-const mention = getByStrings('channelWithIcon', { defaultExport: false })
-
 const debounceTimeMs = 1000
 
+const searchOptions = { walkable: ['children', 'props'] } // For Utils.findInTree
+const { Webpack, Patcher, Utils, Data, UI } = new BdApi('BetterChatNames')
+const { waitForModule, getByStrings, getByKeys, getByPrototypeKeys } = Webpack
+const { findInTree } = Utils
+const modules = {}
+let titleObserver
+
 module.exports = class BetterChatNames {
-    start() {
+    async start() {
         this.settings = Object.assign({}, defaultSettings, Data.load('settings'))
+        await this.getModules()
+
         this.updateOverrideMap()
         this.observeAppTitle()
         this.patchNames()
@@ -109,12 +102,23 @@ module.exports = class BetterChatNames {
                 }, debounceTimeMs)
                 return
             }
-            
+
             this.settings[id] = value
             Data.save('settings', this.settings)
             this.refreshChannel()
         }
     })
+
+    async getModules() {
+        modules.currentServer = getByKeys('getLastSelectedGuildId')
+        modules.currentChannel = getByKeys('getLastSelectedChannelId')
+        modules.transitionTo = getByStrings('transitionTo - Transitioning to', { searchExports: true })
+
+        modules.sidebar = await waitForModule(m => m?.render?.toString()?.includes('.CHANNEL'))
+        modules.header = getByStrings('.HEADER_BAR)', { defaultExport: false })
+        modules.placeholder = getByPrototypeKeys('getPlaceholder').prototype
+        modules.mention = getByStrings('channelWithIcon', { defaultExport: false })
+    }
 
     observeAppTitle() {
         let lastUnpatchedAppTitle
@@ -130,7 +134,7 @@ module.exports = class BetterChatNames {
     patchAppTitle() {
         const patchedTitle = this.patchText(document.title)
 
-        if (currentServer?.getGuildId() && document.title !== patchedTitle) // If in server and title not already patched
+        if (modules.currentServer?.getGuildId() && document.title !== patchedTitle) // If in server and title not already patched
             document.title = patchedTitle
     }
 
@@ -142,7 +146,7 @@ module.exports = class BetterChatNames {
     }
 
     patchSidebar() {
-        Patcher.after(sidebar, 'render', (_, __, data) => {
+        Patcher.after(modules.sidebar, 'render', (_, __, data) => {
             const channelName = findInTree(data, prop => prop?.name, searchOptions)
             const channelType = findInTree(data, prop => prop?.channel, searchOptions).channel.type
 
@@ -152,7 +156,7 @@ module.exports = class BetterChatNames {
     }
 
     patchHeader() {
-        Patcher.after(header, 'A', (_, __, data) => {
+        Patcher.after(modules.header, 'A', (_, __, data) => {
             const rootChannel = findInTree(data, prop => prop?.level, searchOptions)
             if (!rootChannel)
                 return
@@ -170,7 +174,7 @@ module.exports = class BetterChatNames {
     }
 
     patchChatPlaceholder() {
-        Patcher.after(placeholder, 'render', (_, __, data) => {
+        Patcher.after(modules.placeholder, 'render', (_, __, data) => {
             const textArea = findInTree(data, prop => prop.channel, searchOptions)
             const isThread = textArea.channel.type === unrestrictedChannels.thread
 
@@ -182,7 +186,7 @@ module.exports = class BetterChatNames {
     }
 
     patchMention() {
-        Patcher.after(mention, 'A', (_, __, data) => {
+        Patcher.after(modules.mention, 'A', (_, __, data) => {
             const channelName = findInTree(data, prop => typeof prop.children === 'string', searchOptions)
 
             if (data.props.className.includes('iconMentionText') || this.settings.patchUnrestricted) // If is a normal chat mention (not a thread) or patchUnrestricted is enabled
@@ -221,12 +225,12 @@ module.exports = class BetterChatNames {
     }
 
     refreshChannel() {
-        const currentServerId = currentServer?.getGuildId()
-        const currentChannelId = currentChannel?.getChannelId()
+        const currentServerId = modules.currentServer?.getGuildId()
+        const currentChannelId = modules.currentChannel?.getChannelId()
 
         if (currentServerId) { // If not in a DM
-            transitionTo('/channels/@me')
-            setImmediate(() => transitionTo(`/channels/${currentServerId}/${currentChannelId}`))
+            modules.transitionTo('/channels/@me')
+            setImmediate(() => modules.transitionTo(`/channels/${currentServerId}/${currentChannelId}`))
         }
     }
 
